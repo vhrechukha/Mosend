67ed07f0a75821c121359108df0bb01dcd9583d9
Viktoria Grechukha <vik.coder@gmail.com>
2021-11-29 23:53:58 +0200

    [feat] added swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from './config/app.config.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,15 @@ async function bootstrap() {
   const appConfig: AppConfigService = await Promise.resolve(
     app.resolve(AppConfigService),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Mosend example')
+    .setDescription('The mosend API description')
+    .setVersion('1.0')
+    .addTag('mosend')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(appConfig.get('APP_PORT'));
 }
