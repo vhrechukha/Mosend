9a2f8c2f6e445ae92e074db8aaea210a7e847f99
Viktoria Grechukha <vik.coder@gmail.com>
2021-12-09 02:57:47 +0200

    [fix] wrote custom decorator for checking object on properties

--- src/modules/file/dto/Finalize.dto.ts
+++ src/modules/file/dto/Finalize.dto.ts
@@ -1,8 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
+import {
+  CheckObjectOnSpecificProperties
+} from '../../../common/validationDecorators/CheckObjectOnSpecificProperties.decorator';
 
 export class FinalizeDto {
   // FIX: write custom decorator for checking object
+  @CheckObjectOnSpecificProperties(['Parts', ['PartNumber', 'ETag']])
   @ApiProperty({
     required: true,
     default: 5,
