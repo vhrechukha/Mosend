013edd76d40f57e90521b8372bcd0d5a9e2eee23
Viktoria Grechukha <vik.coder@gmail.com>
2021-12-10 00:02:37 +0200

    [fix] made error handling easier

--- src/common/errors/all-exceptions.filter.ts
+++ src/common/errors/all-exceptions.filter.ts
@@ -5,7 +5,6 @@ import {
   HttpException,
   HttpStatus,
 } from '@nestjs/common';
-import { QueryFailedError } from 'typeorm';
 
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
@@ -14,37 +13,23 @@ export class AllExceptionsFilter implements ExceptionFilter {
     const response = ctx.getResponse();
     const request = ctx.getRequest();
 
-    let message = (exception as any)?.message?.message;
-    let code = 'HttpException';
-    let status: HttpStatus;
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+    const message =
+      response.message ||
+      response.message?.[0] ||
+      exception.response?.message?.[0] ||
+      exception.message ||
+      exception.message?.error ||
+      exception.response?.statusCode ||
+      '';
+    const code =
+      exception?.code ||
+      HttpStatus[status || exception?.response?.statusCode] ||
+      '';
 
-    switch (exception.constructor) {
-      case HttpException:
-        status = (exception as HttpException).getStatus();
-        break;
-      case QueryFailedError:
-        status = HttpStatus.UNPROCESSABLE_ENTITY;
-        message = (exception as QueryFailedError).message;
-        code = (exception as any).code;
-        break;
-      default:
-        status =
-          exception instanceof HttpException
-            ? exception.getStatus()
-            : HttpStatus.INTERNAL_SERVER_ERROR;
-        message =
-          response.message ||
-          response.message?.[0] ||
-          exception.response?.message?.[0] ||
-          exception.message ||
-          exception.message?.error ||
-          exception.response?.statusCode ||
-          '';
-        code =
-          exception?.code ||
-          HttpStatus[status || exception?.response?.statusCode] ||
-          '';
-    }
 
     response.status(status).json({
       status,
