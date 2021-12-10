ee847e71e08b45acffcf2c92b7c1f3c0d28e9287
Viktoria Grechukha <vik.coder@gmail.com>
2021-12-10 21:56:40 +0200

    [feat] added creating of foreign key

--- src/database/migrations/1639011772304-FileTable.ts
+++ src/database/migrations/1639011772304-FileTable.ts
@@ -1,87 +1,96 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import {
+  MigrationInterface, QueryRunner, Table, TableForeignKey,
+} from 'typeorm';
 
 export class Filetable1639011772304 implements MigrationInterface {
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(
-            new Table({
-                name: 'files',
-                columns: [
-                    {
-                        name: 'id',
-                        type: 'int4',
-                        isPrimary: true,
-                        isGenerated: true,
-                        generationStrategy: 'increment',
-                    },
-                    {
-                        name: 'created_at',
-                        type: 'timestamp',
-                        isNullable: false,
-                        default: 'now()',
-                    },
-                    {
-                        name: 'updated_at',
-                        type: 'timestamp',
-                        isNullable: false,
-                        default: 'now()',
-                    },
-                    {
-                        name: 'maxDownloadCount',
-                        type: 'int4',
-                        isNullable: false,
-                    },
-                    {
-                        name: 'lastDownloadAt',
-                        type: 'timestamp',
-                    },
-                    {
-                        name: 'downloadCount',
-                        type: 'int4',
-                    },
-                    {
-                        name: 'user_id',
-                        type: 'int4',
-                    },
-                    {
-                        name: 'chunkSize',
-                        type: 'int4',
-                    },
-                    {
-                        name: 'chunkCount',
-                        type: 'int4',
-                    },
-                    {
-                        name: 'filesize',
-                        type: 'text',
-                    },
-                    {
-                        name: 's3_status',
-                        type: 'text',
-                        default: "'inProgress'"
-                    },
-                    {
-                        name: 's3_path',
-                        type: 'text',
-                    },
-                    {
-                        name: 'contentType',
-                        type: 'text',
-                    },
-                    {
-                        name: 'extension',
-                        type: 'text',
-                    },
-                    {
-                        name: 'filename',
-                        type: 'text',
-                    },
-                ],
-            }),
-            false,
-        );
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'files',
+        columns: [
+          {
+            name: 'id',
+            type: 'int4',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            isNullable: false,
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            isNullable: false,
+            default: 'now()',
+          },
+          {
+            name: 'maxDownloadCount',
+            type: 'int4',
+            isNullable: false,
+          },
+          {
+            name: 'lastDownloadAt',
+            type: 'timestamp',
+          },
+          {
+            name: 'downloadCount',
+            type: 'int4',
+          },
+          {
+            name: 'user_id',
+            type: 'int4',
+          },
+          {
+            name: 'chunkSize',
+            type: 'int4',
+          },
+          {
+            name: 'chunkCount',
+            type: 'int4',
+          },
+          {
+            name: 'filesize',
+            type: 'int4',
+          },
+          {
+            name: 's3_status',
+            type: 'text',
+            default: "'inProgress'",
+          },
+          {
+            name: 's3_path',
+            type: 'text',
+          },
+          {
+            name: 'contentType',
+            type: 'text',
+          },
+          {
+            name: 'extension',
+            type: 'text',
+          },
+          {
+            name: 'filename',
+            type: 'text',
+          },
+        ],
+      }),
+      false,
+    );
 
-    public async down(queryRunner: QueryRunner): Promise<any> {
-        queryRunner.query(`DROP TABLE files`);
-    }
+    await queryRunner.createForeignKey('files', new TableForeignKey({
+      columnNames: ['user_id'],
+      referencedColumnNames: ['id'],
+      referencedTableName: 'users',
+      onDelete: 'CASCADE',
+    }));
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    queryRunner.query('DROP TABLE files');
+  }
 }
