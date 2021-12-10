2f1007b3a7c8c2a03cf648821904956844c2c878
Viktoria Grechukha <vik.coder@gmail.com>
2021-12-10 21:28:44 +0200

    [fix] fixed .eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,8 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb-typescript/base'
   ],
   root: true,
   env: {
@@ -15,6 +17,8 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
