fddbbc0341e1f89127290f4cd12520782853eab8
Viktoria Grechukha <vik.coder@gmail.com>
2021-12-13 17:49:22 +0200

    [fix] little fixes

--- src/common/responses.ts
+++ src/common/responses.ts
@@ -1,7 +1,7 @@
 enum AuthResponse {
-  SuccessfullyVerified = 'You were successfully verified. Yet, you can login with your email and password.',
-  SuccessfullySignedUp = 'You signed up successfully. Please, verify your email at first.',
-  SuccessfullyDeleted = 'User was successfully deleted.',
+  Verified = 'You were successfully verified. Yet, you can login with your email and password.',
+  SignedUp = 'You signed up successfully. Please, verify your email at first.',
+  Deleted = 'User was successfully deleted.',
   PasswordUpdated = 'Password updated successfully.',
   PasswordReset = 'Password reset successfully.',
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -11,41 +11,41 @@ import { User } from './entities/user.entity';
 export class UserService {
   constructor(
     @InjectRepository(User)
-    private usersRepository: Repository<User>,
+    private repository: Repository<User>,
   ) {}
 
   getUsers({ skip = 0, take = 10 }) {
-    return this.usersRepository.find({
+    return this.repository.find({
       skip,
       take,
     });
   }
 
   async save({ password, ...data }) {
-    return this.usersRepository.save({
+    return this.repository.save({
       ...data,
       password: await bcrypt.hash(password, 10),
     });
   }
 
   async updateData(data) {
-    return this.usersRepository.save({ ...data });
+    return this.repository.save({ ...data });
   }
 
   async findOneByEmail(email) {
-    return this.usersRepository.findOne({ email });
+    return this.repository.findOne({ email });
   }
 
   async findOneById(id) {
-    return this.usersRepository.findOne(id);
+    return this.repository.findOne(id);
   }
 
   deleteById(id: number): Promise<DeleteResult> {
-    return this.usersRepository.delete(id);
+    return this.repository.delete(id);
   }
 
   deleteMany(days: number) {
-    return this.usersRepository.delete({
+    return this.repository.delete({
       suspended_at: LessThanOrEqual(new Date(new Date().getTime() - days * 24 * 60 * 60 * 1000)),
     });
   }
